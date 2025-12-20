737423ec0aa5ca4788f86fa8c7d55a37a4e21e21
ahn71 <nayem.codeware@gmail.com>
2025-12-20 22:51:47 +0600

    removed override page for employee doctype

--- cw_erp_washing/overrides/employee.py
+++
@@ -1,8 +0,0 @@
-from erpnext.setup.doctype.employee.employee import Employee
-import frappe
-
-class WashingEmployee(Employee):
-    def validate(self):
-        frappe.msgprint("🔥 WashingEmployee override method WORKING! Save blocked for test")
-        print("🔥 WashingEmployee override method WORKING! Save blocked for test")
-        return   # stops save for testing
